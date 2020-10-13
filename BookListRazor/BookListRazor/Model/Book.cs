2a2a174cefb5e624f54ab7f475463673da939660
Zlatomir Kosev <zlatomir_hk@hotmail.com>
2020-10-13 15:00:25 +0100

    Basic Server Setup
    Whats going on?

---
+++ BookListRazor/BookListRazor/Model/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class Book
+    {
+
+        // Add key as Identity column and create value automatically
+        [Key]
+        public int Id { get; set; }
+
+        //Name cannot be null
+        [Required]
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+    }
+
+    
+}
